b19d159bfb1d2a22a9a7073088e2c368d4fb3161
Nikita Ribakovs <nribakov@c2r4p7.42vienna.com>
2026-06-04 16:27:50 +0200

    Add header file for builtins and split token_processor

---
+++ inc/builtin.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 14:27:57 by nribakov          #+#    #+#             */
+/*   Updated: 2026/05/30 14:31:07 by nribakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
+# define ECHO "echo"
+# define CD "cd"
+# define PWD "pwd"
+# define EXPORT "export"
+# define UNSET "unset"
+# define ENV "env"
+# define EXIT "exit"
+
+#endif
--- srcs/token_processor/token_processor.c
+++ srcs/token_processor/token_processor.c
@@ -5,32 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/01 18:56:10 by nribakov          #+#    #+#             */
-/*   Updated: 2026/06/01 18:56:10 by nribakov         ###   ########.fr       */
+/*   Created: 2026/05/30 13:10:11 by nribakov          #+#    #+#             */
+/*   Updated: 2026/06/04 16:25:03 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "builtin.h"
 #include "minishell.h"
 
-# define ENV "env"
-# define EQUAL 0
+#define EQUAL 0
+
+void	map_to_command(t_ctx *c, char *cmd)
+{
+	if (ft_strncmp(cmd, ENV, 4) == EQUAL)
+	{
+		printf("Will execute the env fun");
+		env(c);
+	}
+}
 
 /*
-TODO: When Bash invokes an external command, the variable ‘$_’ is set to the full pathname of the command and passed to that command in its environment.
+TODO: When Bash invokes an external command,
+	the variable ‘$_’ is set to the full pathname of the command and passed to that command in its environment.
 */
 int	process_token(t_ctx *c, size_t token_idx)
 {
-  t_token	*token;
-  char *content;
+	t_token	*token;
+	char	*content;
 
-  token = get_token_from_idx(&(c->arena[AT_TOKEN]), token_idx);
-  if (token->type == TT_WORD)
-  {
-    content = get_token_content(c, token_idx);
-    printf("\nWill execute builin\n");
-    if(ft_strncmp(content, ENV, 4) == EQUAL)
-     env(c);
-  }
-  
-  return 0;
+	token = get_token_from_idx(&(c->arena[AT_TOKEN]), token_idx);
+	if (token->type == TT_WORD)
+	{
+		content = get_token_content(c, token_idx);
+		printf("\nWill execute builin\n");
+		map_to_command(c, content);
+	}
+	return (0);
 }
