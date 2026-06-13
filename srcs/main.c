851b572133e2b091bad5bfbe568410a8a62d6381
Stefan-Emanuel Ancuta <fainica24@gmail.com>
2026-06-13 08:21:59 +0200

    switch over to new architecture - deleted unnecessary files - updated input and main - added everything to Makefile and updated it's vpath

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 21:47:55 by sancuta           #+#    #+#             */
-/*   Updated: 2026/06/05 19:24:18 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/06/12 17:19:38 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,48 @@ static t_ctx	init_ctx(char **envp)
 	ft_memset(&c, 0, sizeof(t_ctx));
 	c.arena[AT_STRING] = arena_init(ARENA_SIZE, sizeof(char));
 	c.arena[AT_PROMPT] = arena_init(ARENA_SIZE, sizeof(char));
-	c.arena[AT_TOKEN] = arena_init(ARENA_SIZE, sizeof(t_token));
-	c.env.envp = envp;
+	c.arena[AT_STACK] = arena_init(ARENA_SIZE, sizeof(t_symbol));
+//	c.arena[AT_CMD] = arena_init(ARENA_SIZE, sizeof(t_cmd));
+	c.env = init_env(c.env, envp);
 	return (c);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
+	t_ctx	c;
+
 	(void) argc;
 	(void) argv;
-
-	t_ctx	c;
-	size_t	token_idx;
-
 	c = init_ctx(envp);
 	while (true)
 	{
 		if (!get_user_input(&c, INPUT_DEFAULT))
 			break ;
-#ifdef DEBUG
-		print_arena(&c.arena[AT_PROMPT]);
-		print_arena(&c.arena[AT_STRING]);
-#endif
-		while (true)
+		if (!*(c.read_line))
 		{
-			token_idx = get_next_token_idx(&c);
-			if (!token_idx)
-				break;
-			if (!ft_strncmp(c.arena[AT_STRING].buf + get_token_from_idx(&c.arena[AT_TOKEN], token_idx)->content.start, "<<", 2))
-				get_user_input(&c, INPUT_CONTINUATION);
-#ifdef DEBUG
-			print_token(&c, token_idx);
-			print_arena(&c.arena[AT_TOKEN]);
-#endif
-		process_token(&c, token_idx);
+			free(c.read_line);
+			continue ;
 		}
+#ifdef DEBUG
+		fprintf(stderr, "\n--- read_line ---\n");
+		fprintf(stderr, "%s\n", c.read_line);
+		fprintf(stderr, "\n--- prompt arena after get_prompt ---\n");
+		print_arena(&c.arena[AT_PROMPT]);
+#endif
+		t_parser_state	parse;
+
+		parse = parse_input(&c);
+#ifdef DEBUG
+		print_complete_stack(&c, &parse);
+#endif
+		exec_stack(&c, &parse);
 		arena_reset(&c.arena[AT_STRING]);
-		arena_reset(&c.arena[AT_TOKEN]);
+		arena_reset(&c.arena[AT_STACK]);
+		free(c.read_line);
 	}
+	arena_free(&c.arena[AT_STRING]);
+	arena_free(&c.arena[AT_PROMPT]);
+	arena_free(&c.arena[AT_STACK]);
+//	arena_free(&c.arena[AT_CMD]);
 	return (0);
 }
--- srcs/token_processor/token_processor.c
+++ srcs/token_processor/token_processor.c
@@ -6,7 +6,7 @@
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 13:10:11 by nribakov          #+#    #+#             */
-/*   Updated: 2026/06/05 19:26:42 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/06/12 10:23:23 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,43 @@
 
 #define EQUAL 0
 
-void	map_to_command(t_ctx *c, char *cmd)
+static void	map_to_command(t_ctx *c, char *cmd)
 {
 	if (ft_strncmp(cmd, ENV, 4) == EQUAL)
-	{
 		env(c);
-	}
 }
 
-/*
-TODO: When Bash invokes an external command,
-	the variable ‘$_’ is set to the full pathname of the command and passed to that command in its environment.
-*/
-int	process_token(t_ctx *c, size_t token_idx)
+int	process_token(t_ctx *c, t_token *token)
 {
-	t_token	*token;
 	char	*content;
 
-	token = get_token_from_idx(&(c->arena[AT_TOKEN]), token_idx);
-	if (token->token_type == TT_WORD)
+	if (token->type == SYM_TOKEN)
 	{
-		content = get_token_content(c, token_idx);
+		content = c->arena[AT_STRING].buf + token->offset;
 		map_to_command(c, content);
 	}
 	return (0);
+}
+
+void	exec_stack(t_ctx *c, t_parser_state *parse)
+{
+	t_symbol	*sym;
+	t_token		t;
+	t_arena		*stack;
+
+	stack = &c->arena[AT_STACK];
+	sym = get_symbol_from_idx(stack, parse->arena_idx);
+	while (1)
+	{
+		if (sym->type == SYM_TOKEN)
+		{
+			t.offset = sym->offset;
+			t.type = sym->type;
+			t.flags = sym->flags;
+			process_token(c, &t);
+		}
+		if (!sym->prev_symbol)
+			break;
+		sym = get_symbol_from_idx(stack, sym->prev_symbol);
+	}
 }
