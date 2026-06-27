8d8d1b8824ff5b1a03b9a129eca2d5897c0490ea
Stefan-Emanuel Ancuta <fainica24@gmail.com>
2026-06-28 01:37:26 +0200

    feature: add feedback on test OK/KO

--- test/helpers.sh
+++ test/helpers.sh
@@ -2,32 +2,36 @@ _assert_run()
 {
 	local input="$1" scope="$2"
 	local block="$(sed 's/^[[:space:]]*//; s/[[:space:]]*$//' <<< "$3")"
-	local result
+	local result delta
 
 	[ -n "$scope" ] || return 0
 	result=$(printf "\e[200~%s\e[201~\n" "$input" \
 			| ./minishell-debug --no_exec --scope="$scope" 2>/dev/null \
 			| sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
-	diff -u <(printf "%s" "$block") <(printf "%s" "$result")
+	delta="$(diff -u <(printf "%s" "$block") <(printf "%s" "$result"))" \
+		&& echo "OK" || { echo "KO"; echo "$delta"; return 1; }
 }
 
 assert_shell()
 {
-	local input="$1" expected="$2" result=0
+	local input="$1" expected="$2"
 	local scope="" block=""
+	local result=0
 
 	while IFS= read -r line; do
 		case "$line" in
-			TOKENS|REDUCE|COMMAND)
+			TOKENS|SYMBOLS|STACK|COMMAND)
 				_assert_run "$input" "$scope" "$block" || result=1
 				block=""
 				case "$line" in
 					TOKENS)
 						scope="tokens" ;;
-					REDUCE)
-						scope="reducer" ;;
+					SYMBOLS)
+						scope="symbols" ;;
+					STACK)
+						scope="stack" ;;
 					COMMAND)
-						scope="tokens,reducer,stack" ;;
+						scope="command" ;;
 				esac ;;
 			*)
 				block="${block}${line}"$'\n' ;;
--- test/runner.sh
+++ test/runner.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+_run_functions()
+{
+	for func in $(awk '/^test_/ {print $1}' "$1"); do
+		echo "Running: ${func%()}"
+		"${func%()}" || result=1
+	done
+}
+
 run()
 {
 	local result=0;
-	for test_file in test/test_builtin_*; do
-		./$test_file || result=1
+	for test_file in test/test_*; do
+		case $test_file in
+			*builtin*)
+				./$test_file || result=1 ;;
+			*)
+				. ./$test_file
+				_run_functions $test_file ;;
+		esac;
 	done;
-
-	. test/test_parser.sh
-	for func in $(awk '/^test_/ {print $1}' test/test_parser.sh); do
-		"${func%()}" || result=1
-	done
 
 	return $result;
 }
