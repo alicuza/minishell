b9a1bcca7652d7418e046f264e3669a0b158ccb9
Nikita Ribakovs <nribakov@c3r9p6.42vienna.com>
2026-07-16 19:36:06 +0200

    Add validation to export, and printing

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -1,5 +1,36 @@
 #include "minishell.h"
 
+static void	print_val(void *content_void_p)
+{
+	t_env_content	*content;
+
+	content = (t_env_content *)content_void_p;
+	if (content->val == NULL)
+		printf("declare -x %s\n", (char *)content->key, content->val);
+	else
+		printf("declare -x %s=\"%s\"\n", (char *)content->key, content->val);
+}
+
+static bool	is_valid_name(char *name) // TODO accept the empty values; add to env init
+{
+	if (!ft_isalpha(*name) && *name != '_')
+		return (false);
+	name++;
+	while (*name != '\0')
+	{
+		if (!ft_isalnum(*name) && *name != '_')
+			return (false);
+		name++;
+	}
+	return (true);
+}
+
+/*
+All values undergo tilde expansion, parameter and variable expansion,
+	command substitution, arithmetic expansion,
+	and quote removal (see Shell Parameter Expansion).
+	Word splitting and filename expansion are not performed.
+*/
 int	builtin_export(t_ctx *c, t_command_ctx *command_ctx)
 {
 	char		**tmp;
@@ -7,12 +38,24 @@ int	builtin_export(t_ctx *c, t_command_ctx *command_ctx)
 	int			result_code;
 
 	i = 1;
+	if (command_ctx->argc == 1)
+	{
+		ft_lstiter(c->env.vals, &print_val);
+		return (EXIT_SUCCESS);
+	}
 	while (i < command_ctx->argc)
 	{
 		tmp = ft_split_key_value(command_ctx->argv[i], '=');
 		if (tmp == NULL)
 			return (exit_mem_issue());
-		// TODO add validation
+		if (is_valid_name(tmp[0]) == false)
+		{
+			free(tmp[0]);
+			free(tmp[1]);
+			free(tmp);
+			i++;
+			continue ;
+		}
 		result_code = env_add(&c->env, tmp[0], tmp[1]);
 		if (result_code)
 		{
@@ -22,5 +65,5 @@ int	builtin_export(t_ctx *c, t_command_ctx *command_ctx)
 		free(tmp);
 		i++;
 	}
-	return (0);
+	return (EXIT_SUCCESS);
 }
