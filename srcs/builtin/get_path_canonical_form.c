0291467ba0fd5f21de3565e556c9168835e7abc6
Nikita Ribakovs <nribakov@c3r11p7.42vienna.com>
2026-07-16 11:53:33 +0200

    Refactor canonical

--- srcs/builtin/get_path_canonical_form.c
+++ srcs/builtin/get_path_canonical_form.c
@@ -8,7 +8,6 @@ typedef struct s_builder
 {
 	size_t	i_orig;
 	size_t	i_new;
-	size_t	dir_start;
 }			t_builder;
 
 static void	skip_dup_slash(char *curpath, size_t len, t_builder *b)
@@ -18,35 +17,32 @@ static void	skip_dup_slash(char *curpath, size_t len, t_builder *b)
 		b->i_orig++;
 }
 
-static void	process_slash(char *curpath, size_t len, t_builder *b)
+static int	process_slash(char *curpath, size_t len, char *canonical_form,
+		t_builder *b)
 {
-	b->dir_start = b->i_orig;
 	skip_dup_slash(curpath, len, b);
-}
-
-static int	process_dot(char *curpath, char *canonical_form, t_builder *b)
-{
-	if (curpath[b->i_orig + 1] == '\0')
-		b->i_orig += 1;
-	if (curpath[b->i_orig + 1] == '/')
-		b->i_orig += 2;
-	else if (curpath[b->i_orig + 1] == '.')
+	if (curpath[b->i_orig + 1] == '.')
 	{
-		if (curpath[b->i_orig + 2] != '\0' && curpath[b->i_orig + 2] != '/')
-			return (EXIT_FAILURE);
-		if (b->i_new - 1 != 0)
-			canonical_form[b->i_new - 1] = 0;
-		b->i_new = ft_strrchr(canonical_form, '/') - canonical_form + 1;
-		b->i_orig++;
+		if (curpath[b->i_orig + 2] == '\0')
+			b->i_orig += 2;
+		else if (curpath[b->i_orig + 2] == '/')
+			b->i_orig += 3;
+		else if (curpath[b->i_orig + 2] == '.')
+		{
+			if (curpath[b->i_orig + 3] != '\0' && curpath[b->i_orig + 3] != '/')
+				return (EXIT_FAILURE);
+			b->i_new = ft_strrchr(canonical_form, '/') - canonical_form + 1;
+			b->i_orig += 2;
+		}
 	}
-	return (0);
+	canonical_form[b->i_new] = '/';
+	return (EXIT_SUCCESS);
 }
 
 void	init_builder(t_builder *builder)
 {
 	builder->i_orig = 0;
 	builder->i_new = 0;
-	builder->dir_start = -1;
 }
 
 char	*get_path_canonical_form(char *curpath, size_t len)
@@ -61,10 +57,8 @@ char	*get_path_canonical_form(char *curpath, size_t len)
 	while (b.i_orig < len && curpath[b.i_orig])
 	{
 		if (curpath[b.i_orig] == '/')
-			process_slash(curpath, len, &b);
-		else if (curpath[b.i_orig] == '.')
 		{
-			if (process_dot(curpath, canonical_form, &b) == EXIT_FAILURE)
+			if (process_slash(curpath, len, canonical_form, &b) == EXIT_FAILURE)
 				return (NULL);
 			continue ;
 		}
--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -2,44 +2,48 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2026/06/06 16:21:09 by nribakov          #+#    #+#             */
 /*   Updated: 2026/06/18 20:46:54 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "env.h"
+#include "minishell.h"
 
-char *get_pwd(t_ctx *c)
+char	*get_pwd(t_ctx *c)
 {
 	char *pwd;
 	pwd = env_get(&c->env, PWD);
-	if(!pwd)
-		pwd = getcwd(NULL,0);
+	if (!pwd)
+		pwd = getcwd(NULL, 0);
 #ifdef DEBUG
 	if (!pwd)
 	{
 		fprintf(stderr, "Error: failed to get cwd");
 	}
 #endif
-	return pwd;
+	return (pwd);
 }
 
 int	pwd(t_ctx *c, t_command_ctx *command_ctx)
 {
-	char* pwd;
+	char *pwd;
 
 #ifdef DEBUG
 	fprintf(stderr, "\nExecuting pwd:\n");
 #endif
-	(void) command_ctx;
+
+	(void)command_ctx;
 	pwd = get_pwd(c);
-	if(!pwd)
-		return EXIT_FAILURE;
+	if (!pwd)
+		return (EXIT_FAILURE);
 	printf("%s\n", pwd);
 	free(pwd);
-	return 0;
+	return (EXIT_SUCCESS);
 }
