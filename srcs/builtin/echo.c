9c6b52f5dd69253e4fa50f721bf5fbea0ecc5b67
Nikita Ribakovs <nribakov@c3r4p1.42vienna.com>
2026-07-24 11:41:57 +0200

    Fix segfalt

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -1,18 +1,35 @@
 #include "minishell.h"
 
-int	echo(t_ctx *c, t_command_ctx *command_ctx) //TODO fix seg fault
+
+static bool	is_empty(char *str)
 {
-  char	*str;
-  char *tmp;
-  
-	str = command_ctx->argv[1];
+	return (str == NULL || str[0] == '\0');
+}
+
+static void	print_val(char *str)
+{
+	if (is_empty(str))
+		printf("\n");
+	else
+		printf("%s\n", str);
+}
+
+int	echo(t_ctx *c, t_command_ctx *command_ctx)
+{
+	char *str;
+	char *tmp;
+
+	if (command_ctx->argc == 1)
+		str = "";
+	else
+		str = command_ctx->argv[1];
 	if (str[0] == '$')
 	{
-		tmp = env_get(&c->env, str + 1); 
-    printf("%s\n", tmp);
-    free(tmp);
-    return 0;
+		tmp = env_get(&c->env, str + 1);
+		print_val(tmp);
+		free(tmp);
+		return (0);
 	}
-  printf("%s\n", str);
-  return 0;
+	print_val(str);
+	return (0);
 }
