ea821ddefceb930f1f9e62582237ba5ab89fe06d
Nikita Ribakovs <nribakov@c2r7p4.42vienna.com>
2026-08-05 15:56:47 +0200

    Start refactor

--- srcs/token_processor/token_processor.c
+++ srcs/token_processor/token_processor.c
@@ -6,7 +6,7 @@
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 13:10:11 by nribakov          #+#    #+#             */
-/*   Updated: 2026/08/04 01:37:37 by nribakov         ###   ########.fr       */
+/*   Updated: 2026/08/05 15:55:31 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,69 +67,30 @@ int	command_search_and_execution(t_ctx *c, t_command_ctx *cmd_ctx)
 		return (execute_non_builtin(c, cmd_ctx));
 }
 
-int	init_command(t_command_ctx *command, uint64_t argc)
-{
-	command->pathname = NULL;
-	command->argc = argc;
-	command->argv = malloc(sizeof(char *) * (argc + 1));
-	if (command->argv == NULL)
-		return (exit_mem_issue());
-	command->argv[argc] = NULL;
-	return (EXIT_SUCCESS);
-}
-
-// TODO "export " will create env with empty key
-// TODO we have SYM_NEWLINE as last one, -1 to not cout it into argc
-int	build_command(t_ctx *c, t_parser_state *parse)
-{
-	t_symbol		*sym;
-	uint64_t		stack_idx;
-	t_arena			*stack;
-	t_command_ctx	command;
-	t_token			*token;
-
-	stack = &c->arena[AT_STACK];
-	if (init_command(&command, parse->stack_idx - 1))
-		return (EXIT_FAILURE);
-	stack_idx = 1;
-	sym = get_ptr_from_idx(stack, stack_idx);
-	while (sym && sym->type == SYM_WORD)
-	{
-		token = get_ptr_from_idx(&c->arena[AT_TOKENS], sym->token_idx);
-		if (command.pathname == NULL)
-		{
-			command.pathname = ft_strdup(c->arena[AT_STRING].buf
-					+ token->offset);
-			if (command.pathname == NULL)
-				return (EXIT_FAILURE);
-		}
-		command.argv[stack_idx - 1] = c->arena[AT_STRING].buf + token->offset;
-		stack_idx++;
-		sym = get_ptr_from_idx(stack, stack_idx);
-	}
-	c->return_status = command_search_and_execution(c, &command);
-	free(command.pathname);
-	free(command.argv);
-	return (EXIT_SUCCESS);
-}
 /*
 TODO nik: Maybe part of reduction:
   - read 2.6.6 Pathname Expansion
-	In the case of a simple command, the results usually include a list of pathnames and fields to be treated as a command name and arguments; see 2.9 Shell Commands.
+	In the case of a simple command,
+		the results usually include a list of pathnames and fields to be treated as a command name and arguments; see 2.9 Shell Commands.
 
 The shell performs redirection (see 2.7 Redirection) and removes redirection operators and their operands from the parameter list.
 
-The shell executes a function (see 2.9.5 Function Definition Command), built-in (see 2.15 Special Built-In Utilities), executable file, or script, giving the names of the arguments as positional parameters numbered 1 to n, and the name of the command (or in the case of a function within a script, the name of the script) as special parameter 0 (see 2.9.1.4 Command Search and Execution).
+The shell executes a function (see 2.9.5 Function Definition Command),
+	built-in (see 2.15 Special Built-In Utilities), executable file, or script,
+	giving the names of the arguments as positional parameters numbered 1 to n,
+	and the name of the command (or in the case of a function within a script,
+	the name of the script) as special parameter 0 (see 2.9.1.4 Command Search and Execution).
 
 The shell optionally waits for the command to complete and collects the exit status (see 2.8.2 Exit Status for Commands).
 */
 void	execute(t_ctx *c, t_parser_state *parse)
 {
-	t_arena		*stack;
-	t_symbol	*symbol;
-	t_node		*pipeline_node;
-	t_node		*command_node;
-	t_node		*arg_node;
+	t_arena			*stack;
+	t_symbol		*symbol;
+	t_node			*pipeline_node;
+	t_node			*command_node;
+	t_node			*arg_node;
+	t_command_ctx	*command;
 
 	stack = &(c->arena[AT_STACK]);
 	symbol = get_ptr_from_offset(stack, stack->offset - stack->stride);
@@ -139,7 +100,10 @@ void	execute(t_ctx *c, t_parser_state *parse)
 		command_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 				pipeline_node->data.command_head_idx);
 		arg_node = command_node->data.command.arg_head;
-		// build_command(c, arg_node);
+		command = build_command(c, arg_node);
+		c->return_status = command_search_and_execution(c, command);
+		free(command->pathname);
+		free(command->argv);
 		pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 				pipeline_node->data.next_idx);
 	}
