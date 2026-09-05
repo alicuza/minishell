38a40767716d3311d2547bb5be3eccacf7374770
Nikita Ribakovs <nribakov@c3r2p1.42vienna.com>
2026-09-06 00:01:40 +0200

    Fix list processing

--- srcs/execute/execute_list.c
+++ srcs/execute/execute_list.c
@@ -6,7 +6,7 @@
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 13:10:11 by nribakov          #+#    #+#             */
-/*   Updated: 2026/09/05 23:41:48 by nribakov         ###   ########.fr       */
+/*   Updated: 2026/09/06 00:00:22 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 static t_node	*get_next_after_or(t_ctx *c, t_node *current_pipeline_node)
 {
 	t_node	*pipeline_node;
-	bool	found;
 
-	found = false;
 	pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 			current_pipeline_node->next_idx);
-	while (pipeline_node->type == NODE_PIPELINE && !found)
+	while (pipeline_node->type == NODE_PIPELINE
+		&& !((pipeline_node->flags & FLAG_OR_IF)))
 	{
-		if (pipeline_node->flags & FLAG_OR_IF)
-			found = true;
 		pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 				current_pipeline_node->next_idx);
 	}
@@ -33,31 +30,31 @@ static t_node	*get_next_after_or(t_ctx *c, t_node *current_pipeline_node)
 static t_node	*get_next_after_and(t_ctx *c, t_node *current_pipeline_node)
 {
 	t_node	*pipeline_node;
-	bool	found;
 
-	found = false;
 	pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 			current_pipeline_node->next_idx);
-	while (pipeline_node->type == NODE_PIPELINE && !found)
+	while (pipeline_node->type == NODE_PIPELINE
+		&& !(pipeline_node->flags & FLAG_AND_IF))
 	{
-		if (pipeline_node->flags & FLAG_AND_IF)
-			found = true;
 		pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 				current_pipeline_node->next_idx);
 	}
 	return (pipeline_node);
 }
 
-static t_node	*get_next(t_ctx *c, t_node *pipeline_node)
+static t_node	*get_next(t_ctx *c, t_node *current_pipeline_node)
 {
+	t_node	*pipeline_node;
+
+	pipeline_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
+			current_pipeline_node->next_idx);
 	if ((!(pipeline_node->flags & FLAG_AND_IF)
 			&& !(pipeline_node->flags & FLAG_OR_IF))
 		|| ((pipeline_node->flags & FLAG_AND_IF)
 			&& c->return_status == EXIT_SUCCESS)
 		|| ((pipeline_node->flags & FLAG_OR_IF)
 			&& c->return_status != EXIT_SUCCESS))
-		return (get_ptr_from_idx(&c->arena[AT_COMMAND],
-				pipeline_node->next_idx));
+		return (pipeline_node);
 	else if (pipeline_node->flags & FLAG_AND_IF)
 		return (get_next_after_or(c, pipeline_node));
 	else
