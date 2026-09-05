44cb1134b88999dc7d165bc4f8a09f63019a20ce
Nikita Ribakovs <nribakov@c3r2p1.42vienna.com>
2026-09-05 23:47:59 +0200

    List processing

--- srcs/execute/execute_list.c
+++ srcs/execute/execute_list.c
@@ -6,9 +6,11 @@
 /*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 13:10:11 by nribakov          #+#    #+#             */
-/*   Updated: 2026/09/05 23:19:05 by nribakov         ###   ########.fr       */
+/*   Updated: 2026/09/05 23:41:48 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
 
 static t_node	*get_next_after_or(t_ctx *c, t_node *current_pipeline_node)
 {
@@ -46,7 +48,7 @@ static t_node	*get_next_after_and(t_ctx *c, t_node *current_pipeline_node)
 	return (pipeline_node);
 }
 
-static t_node	*get_next(t_ctx *c, t_node *current_pipeline_node)
+static t_node	*get_next(t_ctx *c, t_node *pipeline_node)
 {
 	if ((!(pipeline_node->flags & FLAG_AND_IF)
 			&& !(pipeline_node->flags & FLAG_OR_IF))
@@ -57,9 +59,9 @@ static t_node	*get_next(t_ctx *c, t_node *current_pipeline_node)
 		return (get_ptr_from_idx(&c->arena[AT_COMMAND],
 				pipeline_node->next_idx));
 	else if (pipeline_node->flags & FLAG_AND_IF)
-		return (get_next_after_or(c,
-				pipeline_node)) else return (get_next_after_and(c,
-				pipeline_node));
+		return (get_next_after_or(c, pipeline_node));
+	else
+		return (get_next_after_and(c, pipeline_node));
 }
 
 // TODO nik: set up https://www.gnu.org/software/bash/manual/bash.html#Signals-1
@@ -74,7 +76,11 @@ void	execute_list(t_ctx *c, uint64_t head_idx)
 		command_node = get_ptr_from_idx(&c->arena[AT_COMMAND],
 				pipeline_node->data.pipeline.command_head_idx);
 		if (command_node->next_idx == 0)
+		{
 			execute_simple_command(c, command_node);
+			if (c->pid_to_wait > 0 && c->return_status == EXIT_SUCCESS)
+				wait_return_status(c);
+		}
 		else
 			execute_pipeline(c, pipeline_node);
 		pipeline_node = get_next(c, pipeline_node);
