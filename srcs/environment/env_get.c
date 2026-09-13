b33bf87a887fd8ef526e1dc7543d4f3a6146b7da
Nikita Ribakovs <nribakov@c3r3p5.42vienna.com>
2026-09-13 20:53:13 +0200

    N /home/nribakov/core/remote/minishell/srcs/environment/env_get.c

--- srcs/environment/env_get.c
+++ srcs/environment/env_get.c
@@ -1,23 +1,40 @@
-#include "minishell.h"
-#define EQUAL 0
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_get.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
+/*   Created: 2026/09/13 20:47:33 by nribakov          #+#    #+#             */
+/*   Updated: 2026/09/13 20:47:33 by nribakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char* env_get(t_env *env, char *key)
+#include "minishell.h"
+
+char	*env_get(t_env *env, char *key)
 {
-	t_list *vals;
-	size_t key_size;
+	t_list	*vals;
+	size_t	key_size;
 
 	vals = env->vals;
 	key_size = ft_strlen(key) + 1;
 	if (vals)
+	{
 		while (vals)
 		{
-			if (ft_strncmp(((t_env_content*) vals->content)->key, key, key_size) == EQUAL)
+			if (!ft_strncmp(((t_env_content *)vals->content)->key, key,
+					key_size))
 			{
-				if(((t_env_content*) vals->content)->val != NULL)
-						return (ft_strdup(((t_env_content*) vals->content)->val));
+				if (((t_env_content *)vals->content)->val != NULL)
+					return (ft_strdup(((t_env_content *)vals->content)->val));
 				return (NULL);
 			}
 			vals = vals->next;
 		}
+	}
 	return (NULL);
 }
