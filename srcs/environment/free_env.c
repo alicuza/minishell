cf598fe064164a122da075e153c97c9ff1a8b57d
Nikita Ribakovs <nribakov@c3r3p5.42vienna.com>
2026-09-13 20:56:28 +0200

    N /home/nribakov/core/remote/minishell/srcs/environment/free_env.c

--- srcs/environment/free_env.c
+++ srcs/environment/free_env.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 20:55:53 by nribakov          #+#    #+#             */
+/*   Updated: 2026/09/13 20:55:53 by nribakov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	free_env_content(void *content_void_p)
 {
-	t_env_content *content;
+	t_env_content	*content;
 
 	content = (t_env_content *)content_void_p;
 	free(content->key);
