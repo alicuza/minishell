ac56e6c776a0f22eb14beb60cb6969260acc040c
Nikita Ribakovs <nribakov@c3r3p1.42vienna.com>
2026-09-14 01:55:16 +0200

    /home/nribakov/core/remote/minishell/srcs/execute/command_search_and_execution.c

--- srcs/execute/command_search_and_execution.c
+++ srcs/execute/command_search_and_execution.c
@@ -2,22 +2,16 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   command_search_and_execution.c                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+
-	+:+     */
-/*   By: sancuta <sancuta@student.42vienna.com>     +#+  +:+
-	+#+        */
-/*                                                +#+#+#+#+#+
-	+#+           */
-/*   Created: 2026/09/13 20:57:11 by sancuta           #+#    #+#             */
-/*   Updated: 2026/09/13 20:57:25 by sancuta          ###   ########.fr       */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nribakov <nribakov@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/14 01:48:49 by nribakov          #+#    #+#             */
+/*   Updated: 2026/09/14 01:48:49 by nribakov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "minishell.h"
-
-#define EQUAL 0
-
 
 static t_command_function	match_builtin(char *name)
 {
@@ -41,7 +35,7 @@ static t_command_function	match_builtin(char *name)
 
 static int	handle_failed_orig_name(t_ctx *c, t_command_ctx *cmd_ctx)
 {
-	t_error e;
+	t_error	e;
 
 	e = (t_error){NULL, strerror(ENOMEM), 1};
 	free(cmd_ctx->pathname);
@@ -50,12 +44,48 @@ static int	handle_failed_orig_name(t_ctx *c, t_command_ctx *cmd_ctx)
 	return (EXIT_FAILURE);
 }
 
+static int	handle_command_not_found(t_ctx *c, char *orig_name,
+		t_node *redir_node)
+{
+	if (process_redirection(c, redir_node) == EXIT_FAILURE)
+		return (free(orig_name), close_io(c), 1);
+	close_io(c);
+	if (orig_name)
+		msh_error(NULL, orig_name, "command not found");
+	else
+		msh_error(NULL, "", "command not found");
+	free(orig_name);
+	return (127);
+}
+
+static int	search_and_execute_non_builtin(t_ctx *c, t_command_ctx *cmd_ctx,
+		t_node *redir_node)
+{
+	char				*orig_name;
+	int					status;
+
+	orig_name = ft_strdup(cmd_ctx->pathname);
+	if (orig_name == NULL)
+		return (handle_failed_orig_name(c, cmd_ctx));
+	status = get_pathname(c, cmd_ctx);
+	if (status == EXIT_FAILURE)
+	{
+		handle_mem_error(c, cmd_ctx, orig_name);
+		return (EXIT_FAILURE);
+	}
+	else if (status == EXIT_SUCCESS && cmd_ctx->pathname != NULL)
+	{
+		free(orig_name);
+		return (execute_non_builtin(c, cmd_ctx, redir_node));
+	}
+	else
+		return (handle_command_not_found(c, orig_name, redir_node));
+}
+
 int	command_search_and_execution(t_ctx *c, t_command_ctx *cmd_ctx,
 		t_node *redir_node)
 {
-	t_command_function command;
-	int status;
-	char *orig_name;
+	t_command_function	command;
 
 	if (ft_strchr(cmd_ctx->pathname, '/') == NULL)
 	{
@@ -63,41 +93,7 @@ int	command_search_and_execution(t_ctx *c, t_command_ctx *cmd_ctx,
 		if (command != NULL)
 			return (execute_builtin(c, cmd_ctx, command, redir_node));
 		else
-		{
-			orig_name = ft_strdup(cmd_ctx->pathname);
-			if (orig_name == NULL)
-			{
-				handle_failed_orig_name(c, cmd_ctx);
-				return (EXIT_FAILURE);
-			}
-			status = get_pathname(c, cmd_ctx);
-			if (status == EXIT_FAILURE)
-			{
-				handle_mem_error(c, cmd_ctx, orig_name);
-				return (EXIT_FAILURE);
-			}
-			else if (status == EXIT_SUCCESS && cmd_ctx->pathname != NULL)
-			{
-				free(orig_name);
-				return (execute_non_builtin(c, cmd_ctx, redir_node));
-			}
-			else
-			{
-				if (process_redirection(c, redir_node) == EXIT_FAILURE)
-				{
-					free(orig_name);
-					close_io(c);
-					return (1);
-				}
-				close_io(c);
-				if (orig_name)
-					msh_error(NULL, orig_name, "command not found");
-				else
-					msh_error(NULL, "", "command not found");
-				free(orig_name);
-				return (127);
-			}
-		}
+			return (search_and_execute_non_builtin(c, cmd_ctx, redir_node));
 	}
 	else
 		return (execute_non_builtin(c, cmd_ctx, redir_node));
